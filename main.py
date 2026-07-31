c2b7d3f2b6395cee973c0c2896e046cbeae34b28
Doruk Ulug <dorukulugplus@gmail.com>
2026-07-31 21:10:27 +0200

    First draft of Calender Agent

--- main.py
+++ main.py
@@ -29,11 +29,54 @@ class Agent:
         return self.response
 
 
+class GoogleCalenderAgent(Agent):
+    def __init__(self, role, job, output_format, response=""):
+        super().__init__(role, job, output_format, response)
+
+    def assign_action(self, user_prompt):
+        final_prompt = f"""
+            Role: {self.role}
+            Job: Determine the action needed to complete this event
+            Output Format: {self.output_format}
+            User Prompt: {user_prompt}
+        """
+
+        self.response = client.responses.create(
+            model="gpt-4o-mini",
+            input=final_prompt
+        )
+
+        print(self.response.output_text.lower())
+
+        if "schedule a meeting" in self.response.output_text.lower():
+            print("Has Meeting")
+            self.create_meeting()
+
+    def create_meeting(self):
+        print(f"Meeting created")
+
+
 agent_role = "Chief Google Tools Manager"
 agent_task = "Determine which Google Tools shall be used for helping the user"
 agent_output_format = """
     tools = [] # List of strings
 """
 agent = Agent(agent_role, agent_task, agent_output_format)
-agent_response = agent.act(input("What can I help you with? "))
+
+calender_agent = GoogleCalenderAgent("Google Calender Manager",
+                                     "Managing Actions in Google Calender",
+                                     "Action: String")
+
+input_prompt = input("What can I help you with? ")
+
+agent_response = agent.act(input_prompt)
+
 print(agent_response.output_text)
+
+if "Google Calendar" in agent_response.output_text:
+    print("Has Google Calendar")
+    calender_agent.assign_action(input_prompt)
+    print(calender_agent.response.output_text)
+else:
+    print("Has no Google Calendar")
+
